84b2f4dbccdfa375d7624cfc88bd8542796b9c01
Lady Bird <stefanyfer.sf@outlook.com>
2019-12-19 13:25:25 -0300

    controlador e rede
    adicionando arquivos de controlador e rede

---
+++ controller.py
@@ -0,0 +1,125 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+# As regras abaixo funcionam para uma rede de 2 switches
+# sendo que cada switch possui um host ligado a si
+
+TOPOLOGIA = {
+	"00:00:00:00:00:01": {
+        "00:00:00:00:00:02": {
+            1: {'in_port': 1, 'out_port': 2},
+            2: {'in_port': 1, 'out_port': 2},
+        },
+        # "00:00:00:00:00:03": {
+        #     1: {'in_port': 1, 'out_port': 2},
+        #     2: {'in_port': 1, 'out_port': 3},
+        # }
+    },
+    "00:00:00:00:00:02": {
+        "00:00:00:00:00:01": {
+            1: {'in_port': 2, 'out_port': 1},
+            2: {'in_port': 2, 'out_port': 1},
+        },
+    },
+    # "00:00:00:00:00:03": {
+    #     "00:00:00:00:00:01": {
+    #         1: {'in_port': 2, 'out_port': 1},
+    #         2: {'in_port': 3, 'out_port': 1},
+    #     },
+    # },
+}
+
+
+class SimpleScenario13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleScenario13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+
+	# adiciona o fluxo na tabela
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+
+        # cria um "flow" a partir da topologia de rede
+        dst = eth.dst # nó de destino
+        src = eth.src # nó de origem
+        dpid = datapath.id # identificador do switch 
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        if src in TOPOLOGIA:
+            if dst in TOPOLOGIA[src]:
+                if dpid in TOPOLOGIA[src][dst]:
+                    if in_port == TOPOLOGIA[src][dst][dpid]['in_port']:
+                        match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst)
+                        out_port = TOPOLOGIA[src][dst][dpid]['out_port']
+                        actions = [parser.OFPActionOutput(out_port)]
+                        # verifica se existe um id válido, para evitar o envio de
+                        # flow_mod e packet_out
+                        if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                            return
+                        else:
+                            self.add_flow(datapath, 1, match, actions)
+
+                        # envia o pacote agora, pelo controlador. o proximo será encaminhado normalmente pelo switch
+                        # isso eh usado para evitar que o primeiro pacote seja perdido, pois não existe 
+                        # uma regra criada
+                        data = None
+                        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                            data = msg.data
+                        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                                  in_port=in_port, actions=actions, data=data)
+                        datapath.send_msg(out)
---
+++ rede.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+from mininet.cli import CLI
+from mininet.log import info
+from mininet.net import Mininet
+from mininet.node import OVSSwitch, RemoteController
+from mininet.topo import Topo
+
+
+class SimpleScenarioTopo(Topo):
+
+    def __init__(self):
+        info("Criando uma rede personalizada")
+
+        # Initialize topology
+        Topo.__init__(self)
+
+        info("*** Using Openflow 1.3 \n")
+        protocols = "OpenFlow13"
+
+		# inicia a criação da rede
+        net = Mininet(switch=OVSSwitch)
+
+        info("*** Creating (reference) controllers\n")
+        # cria o controlador que será utilizado na rede
+        c1 = net.addController('c1', controller=RemoteController, ip='127.0.0.1', port=6633)
+
+        info("*** Criando os switches\n")
+        s1 = net.addSwitch('s1', protocols=protocols)
+        s2 = net.addSwitch('s2', protocols=protocols)
+
+        info("*** Criando os hosts\n")
+        # cria os hosts 1 e 2
+        h1 = net.addHost(name='h1', ip='10.0.0.1', mac='00:00:00:00:00:01', defaultHost='')
+        h2 = net.addHost(name='h2', ip='10.0.0.2', mac='00:00:00:00:00:02', defaultHost='')
+     
+
+        info("*** Creating links\n")
+        # adiciona a conexao entre o host 1 e o switch 1
+        net.addLink(h1, s1)
+        # adiciona a conexao entre o switch 1 e o switch 2
+        net.addLink(s1, s2)
+        # adiciona a conexao entre o host 2 e o switch 2
+        net.addLink(h2, s2)
+
+        info("*** Criando a rede\n")
+        net.build()
+        # inicia o controlador da rede
+        s1.start([c1])
+        # incializa a rede
+        net.start()
+        net.staticArp()
+        # inicia o CLI do Mininet
+        CLI(net)
+        net.stop()
+
+
+topos = {'simple_scenario_topo': (lambda: SimpleScenarioTopo())}
